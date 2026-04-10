eb689b48c99f300d603d3736989d229cd274c601
est.natanrcs <est.natanrcs@JSMC02F659HQ05N.local>
2026-04-10 13:07:06 -0300

    metendo o biruta com o git

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-def adicao():
+def ad():
     a = float(input('Insert a number: '))
     b = float(input('Insert a number: '))
     c= float(input('Insert a number: '))
     result = a + b + c
     print(f'The result is: {result}')
 
+def sub():
+    a = float(input('Insert a number: '))
+    b = float(input('Insert a number: '))
+    c= float(input('Insert a number: '))
+    result = a - b - c
+    print(f'The result is: {result}')
+
 while True:
     a = int(input('Insert a number: '))
     if a ==1:
         print('hello world')
-        adicao()
+        ad()
     elif a ==2:
         print('hello world')
+        sub()
     elif a ==3:
         print('hello world')
     elif a ==4:
