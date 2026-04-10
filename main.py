33f420dea95ebebd1425edc2a25c786bf23c40eb
est.natanrcs <est.natanrcs@JSMC02F659HQ05N.local>
2026-04-10 13:01:23 -0300

    segundo commits

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+def adicao():
+    a = float(input('Insert a number: '))
+    b = float(input('Insert a number: '))
+    c= float(input('Insert a number: '))
+    result = a + b + c
+    print(f'The result is: {result}')
+
 while True:
     a = int(input('Insert a number: '))
     if a ==1:
         print('hello world')
+        adicao()
     elif a ==2:
         print('hello world')
     elif a ==3:
