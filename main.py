ef40c125785d68d149b5a3abc4561b4a161fe077
est.natanrcs <est.natanrcs@JSMC02F659HQ05N.local>
2026-04-10 12:36:03 -0300

    primeiro commits

---
+++ main.py
@@ -0,0 +1,17 @@
+while True:
+    a = int(input('Insert a number: '))
+    if a ==1:
+        print('hello world')
+    elif a ==2:
+        print('hello world')
+    elif a ==3:
+        print('hello world')
+    elif a ==4:
+        print('hello world')
+    elif a ==5:
+        print('hello world')
+    elif a ==0:
+        print('Exit system')
+        break
+    else:
+        print('Invalid number!')
