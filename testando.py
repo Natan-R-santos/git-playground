f0ca9a4f162c97ab51a04f197a0f6917d9d3c6fb
est.natanrcs <est.natanrcs@JSMC02F659HQ05N.local>
2026-05-04 11:17:00 -0300

    Fiz um teste no arquivo e deu bom

--- testando.py
+++ testando.py
@@ -1,1 +1,9 @@
 print("Este repo é pra meter o biruta com o git,aprender na pratica a linguica toda hehe")
+
+while True:
+    option= input("Teste Teste: ")
+    if option == "1":
+        print(f"Deu bom o teste")
+    else:
+        print("saindo da linguica...")
+        break
