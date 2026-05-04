7f7ae4fc53fc60b3d8ff86eed88af7ec3e707db9
est.natanrcs <est.natanrcs@JSMC02F659HQ05N.local>
2026-05-04 10:37:37 -0300

    add codigo js hahaha

---
+++ teste.js
@@ -0,0 +1,25 @@
+console.log("testando a coisa toda")
+console.log("testando a coisa toda")
+console.log("testando a coisa toda")
+console.log("testando a coisa toda")
+console.log("testando a coisa toda")
+console.log("testando a coisa toda")
+console.log("testando a coisa toda")
+console.log("testando a coisa toda")
+const name="Dev Natanrcs"
+let msg="belo dia pra meter o biruta com o",{name};
+console.log("testando a coisa toda")
+console.log("testando a coisa toda")
+console.log("testando a coisa toda")
+console.log("testando a coisa toda")
+console.log("testando a coisa toda")
+console.log("testando a coisa toda")
+console.log("testando a coisa toda")
+console.log("testando a coisa toda")
+console.log("testando a coisa toda")
+console.log("testando a coisa toda") 
+function saudacao(name,value) {
+    let msg = `Bom dia magnata, ${name} seu saldo é de ${value}`;
+    return msg;
+}
+console.log(saudacao("Natan, 482.371.928.00"));
