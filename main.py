39f6c13bcdfe4a23d9f042314c2cca7f2bdf483f
Natan Santos <nathanrcs2022@gmail.com>
2026-08-30 21:37:32 -0300

    treinando testes com unittest nativo do python

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
-def something(something:str):
-    if something.title() == "Natan":
-        return f"Hello Dev {something}"
-    return "Gabi"
+import unittest
+
+def soma(a,b):
+    return a +b
+
+def something(name:str):
+    msg = f"Hello Dev: {name}, welcome"
+    return msg
+
+class TestSoma(unittest.TestCase):
+    def test_soma_num_positive(self):
+        num1 = 100
+        num2 = 100
+        result = soma(num1,num2)
+        self.assertEqual(result,200)
+
+    def test_soma_num_negative(self):
+        num1 = -1
+        num2 = 1
+        result = soma(num1,num2)
+        self.assertEqual(result,0)
+
+class TestSomething(unittest.TestCase):
+    def test_something_name(self):
+        name = "Natan"
+        result = something(name)
+        self.assertEqual(result,"Hello Dev: Natan, welcome")
+    
+if __name__ == "__main__":
+    unittest.main()
+
+
+
--- test_main.py
+++ test_main.py
@@ -1,8 +1,24 @@
 import unittest
-from main import something
+from main import soma, something
+
+class TestSoma(unittest.TestCase):
+    def test_soma_num_positive(self):
+        num1 = 100
+        num2 = 100
+        result = soma(num1,num2)
+        self.assertEqual(result,200)
+
+    def test_soma_num_negativo(self):
+        num1 = -1
+        num2 = 10
+        result = soma(num1,num2)
+        self.assertEqual(result,0)
 
 class TestSomething(unittest.TestCase):
-    def test_returns_dev_prefix_for_natan(self):
-        args_test = "Natan" 
-        result = something(args_test)
-        self.assertEqual("Hello Dev Natan", result)
+    def test_something_name(self):
+        name = "Natan"
+        result = something(name)
+        self.assertEqual(result,"Hello Dev: Natan, welcome")
+    
+if __name__ == "__main__":
+    unittest.main()
